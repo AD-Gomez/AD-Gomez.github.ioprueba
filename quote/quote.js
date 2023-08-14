d11bbae3c76aec3f7bab32f191f16005a25d74e7
Nairelys Hernandez <njhs694@gmail.com>
2023-08-14 10:49:38 -0400

    redirection

--- quote/quote.js
+++ quote/quote.js
@@ -40,15 +40,12 @@ function maskPhone() {
 
 (function () {
   window.onload = function () {
-    console.log("LOAD");
     initForm();
 
     maskPhone();
 
-    console.log("load form");
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName("needs-validation");
-    console.log(forms, "FORMS");
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function (form) {
       form.addEventListener(
@@ -319,6 +316,7 @@ function sendLead(data) {
       location.href = "../quote2/index.html";
     })
     .catch((err) => {
-      alert("Unexpected error, try again later");
+      // alert("Unexpected error, try again later");
+      location.href = "../quote2/index.html";
     });
 }
